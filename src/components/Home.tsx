8ea6c8894b53bf286d52fe36567e92aecfdb1917
Jabuf <jason.buffet@gmail.com>
2023-08-01 12:55:37 +0200

    add an api call

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import '../styles/Home.css';
-import {Button} from "@mui/material";
+import React, { useState } from 'react'
+import '../styles/Home.css'
+import { Button } from '@mui/material'
+import CreateSheetsFile from '../api/CreateSheetsFile'
 
 function Home() {
-    return (
-        <div className="Home">
-            <header className="App-header">
-                <Button variant="contained">Generate file</Button>
-            </header>
-        </div>
-    );
+  const [data, setData] = useState([])
+
+  const handleApiCall = (apiData: React.SetStateAction<never[]>) => {
+    setData(apiData)
+  }
+
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <Button
+          onClick={() => CreateSheetsFile({ onApiCall: handleApiCall })}
+          variant="contained"
+        >
+          Generate file
+        </Button>
+        {data.length > 0 && (
+          <div>
+            <h2>API Data</h2>
+          </div>
+        )}
+      </header>
+    </div>
+  )
 }
 
-export default Home;
+export default Home

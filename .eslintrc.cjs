8ffa84bfbc271e0c45f7aea43ad3c21e7318bc7f
Jabuf <jason.buffet@gmail.com>
2023-08-01 11:08:04 +0200

    add config for linting and axios package

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier"
+      ],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"]
+      }
+    }
+  ],
+  rules: {
+    'no-console': 'error',
+    semi: ['error', 'never']
+  },
+  root: true
+};
